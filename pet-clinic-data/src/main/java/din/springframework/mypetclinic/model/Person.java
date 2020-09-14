7abfb6ca01ff1d626d2e60646a2c64005b13e55a
dinpwr <58038112+dinpwr@users.noreply.github.com>
2020-09-14 16:14:38 +0300

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/din/springframework/mypetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/din/springframework/mypetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package din.springframework.mypetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/din/springframework/mypetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/din/springframework/mypetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package din.springframework.mypetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
