111bc3fc3d74b94021a8f626f2b645e3b04edcbc
dinpwr <58038112+dinpwr@users.noreply.github.com>
2020-09-14 16:06:22 +0300

    Implement Interfaces for services. closes #5

---
+++ pet-clinic-data/src/main/java/din/springframework/mypetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package din.springframework.mypetclinic.services;
+
+import din.springframework.mypetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
