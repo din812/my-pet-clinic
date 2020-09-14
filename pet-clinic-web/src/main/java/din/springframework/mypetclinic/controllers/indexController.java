45ea0376a434fdfe7c34e423e747051483ea2ed0
dinpwr <58038112+dinpwr@users.noreply.github.com>
2020-09-14 19:54:38 +0300

    Adding Index Page and Controller. closes #8

---
+++ pet-clinic-web/src/main/java/din/springframework/mypetclinic/controllers/indexController.java
@@ -0,0 +1,13 @@
+package din.springframework.mypetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"", "/", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
