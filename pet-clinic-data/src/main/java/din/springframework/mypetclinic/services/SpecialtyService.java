9ae7c105abdceee69c6476d981aec4b6d5ff779b
dinpwr <58038112+dinpwr@users.noreply.github.com>
2020-09-16 17:55:59 +0300

    Added SpecialtyService.java and SpecialtyServiceMap.java. closes #31

---
+++ pet-clinic-data/src/main/java/din/springframework/mypetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package din.springframework.mypetclinic.services;
+
+import din.springframework.mypetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
