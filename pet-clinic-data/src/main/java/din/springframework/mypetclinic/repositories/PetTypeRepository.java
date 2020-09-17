8ce6b7a71df93b5180c4c8780de5dec8e4d7018c
dinpwr <58038112+dinpwr@users.noreply.github.com>
2020-09-17 20:29:17 +0300

    added spring data jpa service for PetTyp. closes #43

--- pet-clinic-data/src/main/java/din/springframework/mypetclinic/repositories/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/din/springframework/mypetclinic/repositories/PetTypeRepository.java
@@ -1,7 +1,7 @@
 package din.springframework.mypetclinic.repositories;
 
-import din.springframework.mypetclinic.model.Pet;
+import din.springframework.mypetclinic.model.PetType;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetTypeRepository extends CrudRepository<Pet, Long> {
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
 }
