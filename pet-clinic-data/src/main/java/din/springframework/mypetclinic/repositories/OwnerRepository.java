071b97b108e2af3865216598d6ad1130b19047e3
dinpwr <58038112+dinpwr@users.noreply.github.com>
2020-09-23 19:08:18 +0300

    impl find owner. Closes #54

--- pet-clinic-data/src/main/java/din/springframework/mypetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/din/springframework/mypetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package din.springframework.mypetclinic.repositories;
 import din.springframework.mypetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/din/springframework/mypetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/din/springframework/mypetclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package din.springframework.mypetclinic.services;
 
 import din.springframework.mypetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
